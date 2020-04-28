2a0f0e55d06c352f58548da074397e46389689ef
Giorgi Gviani <giovere@gmail.com>
2020-04-28 11:34:33 +0200

    added logger

--- src/main/java/com/swissbit/keywords/KwsRest.java
+++ src/main/java/com/swissbit/keywords/KwsRest.java
@@ -3,11 +3,17 @@ package com.swissbit.keywords;
 import step.handlers.javahandler.AbstractKeyword;
 import step.handlers.javahandler.Keyword;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class KwsRest extends AbstractKeyword {
+
+    private static final Logger logger = LogManager.getLogger();
 
     @Keyword
     public void dummyKw() {
         String arg = input.getString("arg");
+        logger.info("input argument is: " + arg);
         output.add("arg", arg);
     }
 
